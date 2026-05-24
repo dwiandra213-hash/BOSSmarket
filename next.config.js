c66d9be086f7fc54e43c81804163fa309d705c17
dwiandra213-hash <dwiandra213@gmail.com>
2026-05-24 11:35:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
